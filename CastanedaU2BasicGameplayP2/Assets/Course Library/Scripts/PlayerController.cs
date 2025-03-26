60b27120dd6ba8a4ce363b09eec2a8895c3e2114
ethancastaneda1 <726750@student.dusd.net>
2025-03-26 09:55:35 -0700

    FInished project work on challanges
    challenge 2

--- CastanedaU2BasicGameplayP2/Assets/Course Library/Scripts/PlayerController.cs
+++ CastanedaU2BasicGameplayP2/Assets/Course Library/Scripts/PlayerController.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10f;
     public float xRange = 10;
+    public float zRangeTop = 15;
+    public float zRangeBottom = 0;
     public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
@@ -27,6 +30,20 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        if (transform.position.z > zRangeTop)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRangeTop);
+        }
+        if (transform.position.z < zRangeBottom)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRangeBottom);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //launch a projectile
--- CastanedaU2BasicGameplayP2/Assets/Course Library/Scripts/SpawnManager.cs
+++ CastanedaU2BasicGameplayP2/Assets/Course Library/Scripts/SpawnManager.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+
+    private float spawnRangeX = 20;
+    private float spawnPosZ= 20;
+
+    private float startDelay = 2;
+    private float spawnInterval = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnpos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+        Instantiate(animalPrefabs[animalIndex], spawnpos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
