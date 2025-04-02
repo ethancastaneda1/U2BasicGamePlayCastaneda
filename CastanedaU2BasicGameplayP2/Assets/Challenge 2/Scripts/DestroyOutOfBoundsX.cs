7225eb10bcded076c6b1bad59334a1328464fb87
ethancastaneda1 <726750@student.dusd.net>
2025-04-02 09:56:09 -0700

    work on challange
    work on challange

--- CastanedaU2BasicGameplayP2/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ CastanedaU2BasicGameplayP2/Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class DestroyOutOfBoundsX : MonoBehaviour
 {
-    private float leftLimit = 30;
+    public GameObject ground;
+    private float leftLimit = -30;
     private float bottomLimit = -5;
 
     // Update is called once per frame
     void Update()
     {
         // Destroy dogs if x position less than left limit
-        if (transform.position.x > leftLimit)
+        if (transform.position.x < leftLimit)
         {
             Destroy(gameObject);
         } 
         // Destroy balls if y position is less than bottomLimit
-        else if (transform.position.z < bottomLimit)
+        else if (transform.position.z > bottomLimit)
         {
-            Destroy(gameObject);
+            //Destroy(gameObject);
         }
 
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+         if(other.gameObject == ground)
+         {
+            Destroy (gameObject);
+         }
     }
 }
