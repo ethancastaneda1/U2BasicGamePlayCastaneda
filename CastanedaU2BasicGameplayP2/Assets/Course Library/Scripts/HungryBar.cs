b631b99b28d15d49b9dd932973d54cc383201322
ethancastaneda1 <726750@student.dusd.net>
2025-04-08 09:37:56 -0700

    work
    work

--- CastanedaU2BasicGameplayP2/Assets/Course Library/Scripts/HungryBar.cs
+++ CastanedaU2BasicGameplayP2/Assets/Course Library/Scripts/HungryBar.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HungryBar : MonoBehaviour
 {
+    public Slider hungerSlider;
+    public int amountToBeFed;
+
+    private int currentFedAmount = 0;
+      
     public 
     // Start is called before the first frame update
     void Start()
